817f48327f9d578439ca42a766f3cf6f587f0114
Jessica <duellje@oregonstate.edu>
2021-05-27 19:17:33 -0400

    got signIn to almost work

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # portfolio-form
 
+Register and login form setup for OSU 493 Portfolio assignment.
+
 ## Project setup
 ```
 npm install
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Welcome to the Plant REST API</h1>
+    <p>
+      Please Sign in to access the Plant REST API
+      <a href="https://osu-493-portfolio.ue.r.appspot.com/plants">here</a>
+    </p>
+    <form @submit="onSubmit">
+      <label for="username">
+        Username
+        <input type="text" id="username" name="username" v-model="username" />
+      </label>
+      <label for="password">
+        Password
+        <input
+          type="password"
+          id="password"
+          name="password"
+          v-model="password"
+        />
+      </label>
+      <button>Submit</button>
+    </form>
+    <div v-if="users.length">
+      <p v-for="user in users" :key="user.id">{{ user }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      username: "",
+      password: "",
+      users: [],
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      let userObj = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .get("http://localhost:5000/users")
+        .then((res) => {
+          this.users = res.data;
+          console.log(res.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+      console.log(userObj);
+    },
   },
 };
 </script>
